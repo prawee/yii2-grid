6dbc3dbb027f23f876cd64801e24e08692d6156a
Prawee Wongsa <prawee@hotmail.com>
2014-07-16 03:53:33 +0700

    Checked urlManager.

--- common/config/main.php
+++ common/config/main.php
@@ -3,7 +3,7 @@
 return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'extensions' => require(__DIR__ . '/../../vendor/yiisoft/extensions.php'),
-    'components' => [
+    'components' => [ 
         'urlMananger' => [
             'class' => 'yii\web\UrlManager',
             'enablePrettyUrl' => true,
