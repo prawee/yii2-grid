f6e69241ff34d3897af3f22b4e64593cbecfc3c9
Prawee Wongsa <prawee@hotmail.com>
2014-07-16 04:27:12 +0700

    Rename urlMananger to urlManager.

--- common/config/main.php
+++ common/config/main.php
@@ -4,7 +4,7 @@ return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'extensions' => require(__DIR__ . '/../../vendor/yiisoft/extensions.php'),
     'components' => [ 
-        'urlMananger' => [
+        'urlManager' => [
             'class' => 'yii\web\UrlManager',
             'enablePrettyUrl' => true,
             'showScriptName' => false,
