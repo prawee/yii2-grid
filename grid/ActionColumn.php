376ed6acba6a124726aaf4a120c94e55bc8c7ee8
Prawee Wongsa <prawee@hotmail.com>
2014-07-18 17:30:37 +0700

    Change button for view.

--- grid/ActionColumn.php
+++ grid/ActionColumn.php
@@ -1,10 +1,42 @@
 <?php
+
 /*
  * 2014-07-18
  * konkeanweb@gmail.com
  */
+
 namespace prawee\grid;
 
-class ActionColumn extends \yii\grid\ActionColumn{
- 
+class ActionColumn extends \yii\grid\ActionColumn {
+
+    protected function initDefaultButtons() {
+        if (!isset($this->buttons['view'])) {
+            $this->buttons['view'] = function ($url, $model) {
+                return Html::a('<span class="glyphicon glyphicon-eye-open"></span>', $url, [
+                            'title' => Yii::t('yii', 'View'),
+                            'class'=>'btn btn-primary',
+                            'data-pjax' => '0',
+                ]);
+            };
+        }
+        if (!isset($this->buttons['update'])) {
+            $this->buttons['update'] = function ($url, $model) {
+                return Html::a('<span class="glyphicon glyphicon-pencil"></span>', $url, [
+                            'title' => Yii::t('yii', 'Update'),
+                            'data-pjax' => '0',
+                ]);
+            };
+        }
+        if (!isset($this->buttons['delete'])) {
+            $this->buttons['delete'] = function ($url, $model) {
+                return Html::a('<span class="glyphicon glyphicon-trash"></span>', $url, [
+                            'title' => Yii::t('yii', 'Delete'),
+                            'data-confirm' => Yii::t('yii', 'Are you sure you want to delete this item?'),
+                            'data-method' => 'post',
+                            'data-pjax' => '0',
+                ]);
+            };
+        }
+    }
+
 }
