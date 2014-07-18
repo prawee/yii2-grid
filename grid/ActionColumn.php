c4e4e8ced1ac12a19858d2941314d6ec33f17fdc
Prawee Wongsa <prawee@hotmail.com>
2014-07-18 18:39:55 +0700

    Finished modified button.

--- grid/ActionColumn.php
+++ grid/ActionColumn.php
@@ -15,27 +15,23 @@ class ActionColumn extends \yii\grid\ActionColumn {
     protected function initDefaultButtons() {
         if (!isset($this->buttons['view'])) {
             $this->buttons['view'] = function ($url, $model) {
-                return Html::a('<span class="glyphicon glyphicon-eye-open"></span>', $url, [
+                return Html::a('<span class="glyphicon glyphicon-eye-open btn btn-xs btn-primary"></span>', $url, [
                             'title' => Yii::t('yii', 'View'),
                             'data-pjax' => '0',
-                            'class' => 'btn btn-xs btn-primary',
-                            'style' => 'margin-right:5px;',
                 ]);
             };
         }
         if (!isset($this->buttons['update'])) {
             $this->buttons['update'] = function ($url, $model) {
-                return Html::a('<span class="glyphicon glyphicon-pencil"></span>', $url, [
+                return Html::a('<span class="glyphicon glyphicon-pencil btn btn-xs btn-warning"></span>', $url, [
                             'title' => Yii::t('yii', 'Update'),
                             'data-pjax' => '0',
-                            'class' => 'btn btn-xs btn-warning',
-                            'style' => 'margin-right:5px;',
                 ]);
             };
         }
         if (!isset($this->buttons['delete'])) {
             $this->buttons['delete'] = function ($url, $model) {
-                return Html::a('<span class="glyphicon glyphicon-trash btn btn-xs btn-danger">Delete</span>', $url, [
+                return Html::a('<span class="glyphicon glyphicon-trash btn btn-xs btn-danger"></span>', $url, [
                             'title' => Yii::t('yii', 'Delete'),
                             'data-confirm' => Yii::t('yii', 'Are you sure you want to delete this item?'),
                             'data-method' => 'post',
