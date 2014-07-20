776ec1171d745327fdd2f2cc1eed6c29a381f90c
Prawee Wongsa <prawee@hotmail.com>
2014-07-20 19:35:55 +0700

    Finished login page.

--- app/views/auth/default/login.php
+++ app/views/auth/default/login.php
@@ -1,7 +1,8 @@
 <?php
+
 use yii\helpers\Html;
-use yii\widgets\ActiveForm;
 use kartik\icons\Icon;
+use yii\bootstrap\ActiveForm;
 
 Icon::map($this);
 
@@ -13,21 +14,40 @@ Icon::map($this);
  */
 $this->title = 'Login';
 $this->params['breadcrumbs'][] = $this->title;
-
 ?>
-<div class="site-login">
-    <h1><?= Html::encode($this->title) ?></h1>
-
+<div class="site-login">  
     <div class="row">
-        <div class="col-lg-5">
-            <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
-                <?= $form->field($model, 'username') ?>
-                <?= $form->field($model, 'password')->passwordInput() ?>
-                <?= $form->field($model, 'rememberMe')->checkbox() ?>
-                <div class="form-group center-block">
-                    <?= Html::submitButton(Icon::show('sign-in').' Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
-                </div>
-            <?php ActiveForm::end(); ?>
+        <?php
+        $form = ActiveForm::begin([
+                    'id' => 'login-form',
+                    'layout' => 'horizontal',
+                    'fieldConfig' => [
+                        'template' => "{label}\n{beginWrapper}\n{input}\n{endWrapper}",
+                        'horizontalCssClasses' => [
+                            'label' => 'col-sm-3',
+                            'offset' => 'col-sm-offset-2',
+                            'wrapper' => 'col-sm-5',
+                            'error' => '',
+                            'hint' => ''
+                        ]
+                    ]
+                ])
+        ?>
+        <div class="col-lg-4 col-lg-offset-2">
+            <div class="login-title">THEOS Programming Tools-2</div>
+
+
+            <?= $form->field($model, 'username') ?>
+            <?= $form->field($model, 'password')->passwordInput() ?>
+            <?php //$form->field($model, 'rememberMe')->checkbox() ?>  
+            <div style="padding-left:100px;">
+                <?= Html::submitButton(Icon::show('sign-in') . ' Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+            </div>
+
         </div>
+        <div class="col-sm-5">
+            <?= $form->errorSummary($model) ?>
+        </div>
+        <?php ActiveForm::end(); ?>
     </div>
 </div>
