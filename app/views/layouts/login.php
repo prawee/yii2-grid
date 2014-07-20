2261273dcb8ef1f307e69cee8c3458c0ed83acbc
Prawee Wongsa <prawee@hotmail.com>
2014-07-20 18:39:32 +0700

    added login layout.

--- app/views/layouts/login.php
+++ app/views/layouts/login.php
@@ -1,9 +1,6 @@
 <?php
 
 use yii\helpers\Html;
-use yii\bootstrap\Nav;
-use yii\bootstrap\NavBar;
-use yii\widgets\Breadcrumbs;
 use app\assets\AppAsset;
 use app\widgets\Alert;
 use kartik\icons\Icon;
@@ -32,14 +29,6 @@ AppAsset::register($this);
                 <?= $content ?>
             </div>
         </div>
-
-        <footer class="footer">
-            <div class="container">
-                <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
-                <p class="pull-right"><?= Yii::powered() ?></p>
-            </div>
-        </footer>
-
         <?php $this->endBody() ?>
     </body>
 </html>
