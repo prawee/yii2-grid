745ed9d4a6e31260e307623f8bc3829e4a7ec328
Prawee Wongsa <prawee@hotmail.com>
2014-07-20 18:47:39 +0700

    added login layout.

--- app/views/layouts/login.php
+++ app/views/layouts/login.php
@@ -21,7 +21,7 @@ AppAsset::register($this);
         <title><?= Html::encode($this->title) ?></title>
         <?php $this->head() ?>
     </head>
-    <body>
+    <body class="login-page">
         <?php $this->beginBody() ?>
         <div class="wrap">
             <div class="container">
