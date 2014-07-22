9668e71902c4a62eecd150b0a0b1cd7d69cc12e4
Prawee Wongsa <prawee@hotmail.com>
2014-07-23 02:45:03 +0700

    updated form

--- app/views/user/update.php
+++ app/views/user/update.php
@@ -1,21 +1,14 @@
 <?php
 
-use yii\helpers\Html;
+//use yii\helpers\Html;
 
-/* @var $this yii\web\View */
-/* @var $model app\models\User */
-
-$this->title = 'Update User: ' . ' ' . $model->id;
-$this->params['breadcrumbs'][] = ['label' => 'Users', 'url' => ['index']];
-$this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
-$this->params['breadcrumbs'][] = 'Update';
+$this->title = 'Update User: ' . ' ' . $model->username;
 ?>
 <div class="user-update">
-
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <?= $this->render('_form', [
+    <?=
+    $this->render('_form', [
         'model' => $model,
-    ]) ?>
+    ])
+    ?>
 
 </div>
