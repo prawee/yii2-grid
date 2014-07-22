b271163821b25026df2a4ef6ad06b12bb466a0f6
Prawee Wongsa <prawee@hotmail.com>
2014-07-23 05:18:50 +0700

    finished administrator

--- app/views/user/administrator.php
+++ app/views/user/administrator.php
@@ -9,13 +9,14 @@ use yii\grid\GridView;
 use yii\bootstrap\Modal;
 use kartik\icons\Icon;
 Icon::map($this);
+use yii\widgets\Pjax;
 
 $this->title = 'Administrator';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <?php
 Modal::begin([
-    'id' => 'content-modal',
+    'id' => 'xs-modal',
     'header' => Icon::show('user') . '<b>Administrator</b>',
     'closeButton' => [
         'aria-hidden' => 'true',
@@ -25,8 +26,9 @@ Modal::begin([
 ?>
 <div class="administrator-index">
     <p>
-        <?= Html::a(Icon::show('plus') . ' Create', ['create'], ['class' => 'btn btn-success']) ?>
+        <?= Html::a(Icon::show('plus') . ' Create', ['create-admin'], ['class' => 'btn btn-success']) ?>
     </p>
+    <?php Pjax::begin(['enablePushState'=>false]); ?>
     <?=
     GridView::widget([
         'dataProvider' => $dataProvider,
@@ -42,17 +44,33 @@ Modal::begin([
             [
                 'class' => 'prawee\grid\ActionColumn',
                 'template' => '{update} {delete}',
-                'options' => ['style' => 'width:100px;'],
+                'options' => ['style' => 'width:70px;'],
+                'buttons'=>[
+                    'update'=>function($url,$data){
+                        return Html::a('<span class="glyphicon glyphicon-edit btn btn-xs btn-warning"></span>',['update-admin','id'=>$data->id],[
+                            'data-pjax'=>'0',
+                            'title'=>'Update',
+                        ]);
+                    },
+                    'delete'=>function($url,$data){
+                        return Html::a('<span class="glyphicon glyphicon-trash btn btn-xs btn-danger"></span>',['delete-admin','id'=>$data->id],[
+                            'data-pjax'=>'0',
+                            'title'=>'Update',
+                        ]);
+                    }
+                ]
             ],
         ],
     ]);
     ?>
+    <?php Pjax::end();?>
     <?=
     Html::a(Icon::show('times') . 'Close', ['/user/index'], [
         'class' => 'btn btn-danger',
         'name' => 'assign-button',
     ]);
     ?>
+    
 </div>
 <?php
 Modal::end();
