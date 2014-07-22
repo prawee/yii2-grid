01c6261c2cebca952e287393336189ec24489c12
Prawee Wongsa <prawee@hotmail.com>
2014-07-22 09:56:46 +0700

    fill data for infomation.

--- app/views/uss/info.php
+++ app/views/uss/info.php
@@ -5,21 +5,22 @@
  */
 
 use yii\widgets\DetailView;
-//use yii\helpers\Html;
+use yii\helpers\Html;
 //use yii\grid\GridView;
 use yii\bootstrap\Modal;
 use kartik\icons\Icon;
+
 Icon::map($this);
 
 
 
-$this->title = 'Source Items';
+$this->title = 'Infomation';
 $this->params['breadcrumbs'][] = $this->title;
 
 
 Modal::begin([
     'id' => 'content-modal',
-    'header' => Icon::show('cog') . '<b>Info</b>',
+    'header' => Icon::show('info') . '<b>Infomation</b>',
     'closeButton' => [
         'aria-hidden' => 'true',
         'class' => 'hide',
@@ -29,26 +30,26 @@ Modal::begin([
 <div class="source-item-index">
     <div class="row">
         <div class="col-xs-6">
+            <b>USS</b>
             <?=
             DetailView::widget([
                 'model' => $model,
                 'attributes' => [
-                    'id',
-                    'aoi_id',
-                    'order_id',
+                    'aoi_name',
+                    //'aoi_id',
+                    //'order_id',
                     'order_doc_no',
                     'order_doc_year',
-                    'order_doc_prefix',
-                    'order_status',
-                    'aoi_name',
+                    //'order_doc_prefix',
+                    //'order_status',
                     'satellite_id',
                     'acq_date_start',
                     'acq_date_end',
-                    'quantity',
-                    'unit',
+                    //'quantity',
+                    //'unit',
                     'remark:ntext',
-                    'attr_ta',
-                    'attr_tl',
+                    //'attr_ta',
+                    ['attribute'=>'attr_tl','label'=>'Priority'],
                     'attr_s',
                     'attr_pt',
                     'attr_ct',
@@ -57,28 +58,36 @@ Modal::begin([
                     'attr_s_id',
                     'attr_pt_id',
                     'attr_ct_id',
-                    'is_ortho',
+                    //'is_ortho',
                     'is_rush',
-                    'is_dem',
-                    'created',
-                    'modified',
+                    //'is_dem',
+                    //'created',
+                    //'modified',
                     'wo_doc_name',
-                    'wo_doc_year',
+                    //'wo_doc_year',
                     'wo_doc_no',
-                    'wo_created',
+                    //'wo_created',
                     'wo_modified',
-                    'tpt_status',
-                    'tpt_user_id',
-                    'tpt_user_name',
-                    'customer_id',
-                    'customer_name',
-                    'customer_name_th',
-                    'project_name',
+                    //'tpt_status',
+                    //'tpt_user_id',
+                    //'tpt_user_name',
+                    //'customer_id',
+                    //'customer_name',
+                    //'customer_name_th',
+                    //'project_name',
                 ],
             ])
             ?>
+            <?=
+            Html::a(Icon::show('times') . 'Close', ['/uss/index'], [
+                'class' => 'btn btn-danger',
+                'name' => 'assign-button',
+            ]);
+            ?>
         </div>
-        <div class="col-xs-6"></div>
+        <div class="col-xs-6">
+            <b>Request</b>
+        </div>
     </div>
 </div>
 <?php
