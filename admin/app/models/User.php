997c433c7bb4d3595abd1bf74e4b4c706b1f9144
Prawee Wongsa <prawee@hotmail.com>
2014-07-22 14:31:53 +0700

    added user model

---
+++ admin/app/models/User.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * 2014-07-15
+ * prawee@hotmail.com
+ */
+namespace admin\app\models;
+
+//use common\models\User as CUser;
+use auth\models\User as CUser;
+
+class User extends CUser{
+    
+}
