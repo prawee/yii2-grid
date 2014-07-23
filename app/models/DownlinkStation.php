22de261802424972c341aabf28273d386d34c6cf
Prawee Wongsa <prawee@hotmail.com>
2014-07-24 02:07:32 +0700

    extends downlink station model.

---
+++ app/models/DownlinkStation.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ * 20140724
+ * prawee@hotmail.com
+ */
+namespace app\models;
+
+use common\models\DownlinkStation as CDownlinkStation;
+class DownlinkStation extends CDownlinkStation{}
+
