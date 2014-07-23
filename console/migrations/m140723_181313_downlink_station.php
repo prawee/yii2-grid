380172cdbeb8f95e1e9b2204128fef9f7b66210b
Prawee Wongsa <prawee@hotmail.com>
2014-07-24 02:00:54 +0700

    added downlink_station migrate table.

--- console/migrations/m140723_181313_downlink_station.php
+++ console/migrations/m140723_181313_downlink_station.php
@@ -22,10 +22,22 @@ class m140723_181313_downlink_station extends Migration {
             'name'=>  Schema::TYPE_STRING.'(128) NOT NULL',
             'value'=> Schema::TYPE_INTEGER,
         ]);
-        $this->insert('downlink_station',[
-            ['name'=>'Bangkok','value'=>'0'],
-            ['name'=>'Miyun','value'=>'1'],
-        ]);
+        $this->insert('downlink_station',['name' => 'Bangkok', 'value' => '0']);
+        $this->insert('downlink_station',['name' => 'Miyun', 'value' => '1']);
+        $this->insert('downlink_station',['name' => 'Kashi', 'value' => '2']);
+        $this->insert('downlink_station',['name' => 'Sanya', 'value' => '3']);
+        $this->insert('downlink_station',['name' => 'Wuhan', 'value' => '4']);
+        $this->insert('downlink_station',['name' => 'Peru', 'value' => '5']);
+        $this->insert('downlink_station',['name' => 'Japan', 'value' => '6']);
+        $this->insert('downlink_station',['name' => 'Kiruna', 'value' => '7']);
+        $this->insert('downlink_station',['name' => 'Canada', 'value' => '8']);
+        $this->insert('downlink_station',['name' => 'Taiwan', 'value' => '9']);
+        $this->insert('downlink_station',['name' => 'Indonesia', 'value' => '10']);
+        $this->insert('downlink_station',['name' => 'Malaysia', 'value' => '11']);
+        $this->insert('downlink_station',['name' => 'Russia1', 'value' => '12']);
+        $this->insert('downlink_station',['name' => 'Russia2', 'value' => '13']);
+        $this->insert('downlink_station',['name' => 'Rissia3', 'value' => '14']);
+        $this->insert('downlink_station',['name' => 'Rissia4', 'value' => '15']);
     }
 
     public function down() {
