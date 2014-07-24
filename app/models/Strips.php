98f5911bf9a2cde09f372c21d493f6d7cae236c6
Prawee Wongsa <prawee@hotmail.com>
2014-07-24 23:50:04 +0700

    added updateXML

--- app/models/Strips.php
+++ app/models/Strips.php
@@ -12,5 +12,28 @@ class Strips extends CStrips{
         $model->save(false);
         return $model->id;
     }
+    public static function updateXML($data,$id){
+        $model = self::findOne($id);
+        echo $model->id;
+        if($model->id){
+            $model->miseo_name=(string)$data->StripName;
+            $model->rank=(int)$data->Rank;
+            $model->lat_center=isset($data->StripCenter->Latitude)?(string)$data->StripCenter->Latitude:null;
+            $model->lon_center=isset($data->StripCenter->Longitude)?(string)$data->StripCenter->Longitude:null;
+            $model->strip_length=isset($data->StripLength)?(string)$data->StripLength:null;
+            $model->strip_width=isset($data->StripWidth)?(string)$data->StripWidth:null;
+            $model->lat_corner_nw=isset($data->CornerNW->Latitude)?(string)$data->CornerNW->Latitude:null;
+            $model->lon_corner_nw=isset($data->CornerNW->Longitude)?(string)$data->CornerNW->Longitude:null;
+            $model->lat_corner_ne=isset($data->CornerNE->Latitude)?(string)$data->CornerNE->Latitude:null;
+            $model->lon_corner_ne=isset($data->CornerNE->Longitude)?(string)$data->CornerNE->Longitude:null;
+            $model->lat_corner_se=isset($data->CornerSE->Latitude)?(string)$data->CornerSE->Latitude:null;
+            $model->lon_corner_se=isset($data->CornerSE->Longitude)?(string)$data->CornerSE->Longitude:null;
+            $model->lat_corner_sw=isset($data->CornerSW->Latitude)?(string)$data->CornerSW->Latitude:null;
+            $model->lon_corner_sw=isset($data->CornerSW->Longitude)?(string)$data->CornerSW->Longitude:null;
+            $model->strip_status_id=(int)$data->StripStatus;
+            $model->save();
+            //print_r($model->errors);
+        }
+    }
 }
 
