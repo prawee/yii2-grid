367e40acd5138338741a1ed142aa59ddcc60f56d
Prawee Wongsa <prawee@hotmail.com>
2014-07-24 21:11:00 +0700

    added isset for check null data.

--- app/models/Databasedata.php
+++ app/models/Databasedata.php
@@ -16,13 +16,13 @@ class Databasedata extends CDatabasedata{
     public static function updateXML($data,$id){
         $model = self::findOne($id);
         if($model->id){
-            $model->name=(string)$data->Name;
-            $model->miseo_reference=(string)$data->ReferenceMiseo;
-            $model->miseo_group=(string)$data->Group;
-            $model->miseo_template=(string)$data->Template;
-            $model->nb_summits_cov=(int)$data->StripNb;
-            $model->stereo_type=(string)$data->StereoType->NA;
-            $model->organism=(string)$data->Organism;
+            $model->name=(string)isset($data->Name);
+            $model->miseo_reference=(string) isset($data->ReferenceMiseo);
+            $model->miseo_group=(string) isset($data->Group);
+            $model->miseo_template=(string)isset($data->Template);
+            $model->nb_summits_cov=(int)isset($data->StripNb);
+            $model->stereo_type=(string)isset($data->StereoType->NA);
+            $model->organism=(string) isset($data->Organism);
             $model->delta_lat_north=isset($data->DeltaLatNorth)?(string)$data->DeltaLatNorth:null;
             $model->delta_lat_south=isset($data->DeltaLatSouth)?(string)$data->DeltaLatSouth:null;
             $model->save();
