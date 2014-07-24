ac8b32ea0d0dc76d87ae37c088220628041a1f9b
Prawee Wongsa <prawee@hotmail.com>
2014-07-24 20:43:47 +0700

    added updateXML

--- app/models/Databasedata.php
+++ app/models/Databasedata.php
@@ -13,4 +13,19 @@ class Databasedata extends CDatabasedata{
         $model->save(false);
         return $model->id;
     }
+    public static function updateXML($data,$id){
+        $model = self::findOne($id);
+        if($model->id){
+            $model->name=(string)$data->Name;
+            $model->miseo_reference=(string)$data->ReferenceMiseo;
+            $model->miseo_group=(string)$data->Group;
+            $model->miseo_template=(string)$data->Template;
+            $model->nb_summits_cov=(int)$data->StripNb;
+            $model->stereo_type=(string)$data->StereoType->NA;
+            $model->organism=(string)$data->Organism;
+            $model->delta_lat_north=isset($data->DeltaLatNorth)?(string)$data->DeltaLatNorth:null;
+            $model->delta_lat_south=isset($data->DeltaLatSouth)?(string)$data->DeltaLatSouth:null;
+            $model->save();
+        }
+    }
 }
