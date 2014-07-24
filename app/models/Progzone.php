c0aed720d80361657416b6f4f10151a52e18240c
Prawee Wongsa <prawee@hotmail.com>
2014-07-24 21:42:51 +0700

    finished updateXML

--- app/models/Progzone.php
+++ app/models/Progzone.php
@@ -6,11 +6,30 @@
 namespace app\models;
 
 use common\models\Progzone as CProgzone;
+use app\models\DownlinkStation;
 class Progzone extends CProgzone{
     public static function insertGetId(){
         $model=new self;
         $model->save(false);
         return $model->id;
+    }
+    public static function updateXML($data,$id){
+        //echo '<pre>'.print_r($data,true).'</pre>';
+        $model = self::findOne($id);
+        if($model->id){
+            $model->miseo_name=(string)$data->ReqName;
+            $model->satellite=(int)$data->ReqSatellite;
+            $model->phase=(int)$data->ReqOrbitPhase;
+            $model->average_altitude=(int)$data->AvgAltitude;
+            $model->miseo_template=(string)$data->Template;
+            $model->center_latitude=(float)isset($data->ReqCenter->Latitude);
+            $model->center_longitude=(float)isset($data->ReqCenter->Longitude);
+            $model->item_length=(int)$data->ReqLength;
+            $model->zonetype=(string)$data->ZoneType;
+            //$model->request_status_id=(int)$data->ReqStatus;
+            //$model->downlink_station_id=(int)DownlinkStation::getIdByValue((int)$data->DownLinkStation);
+            $model->save();
+        }
     }
 }
 
