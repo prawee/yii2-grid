aae945cfd901e4358128263f7f6b96085f181690
Prawee Wongsa <prawee@hotmail.com>
2014-07-24 22:28:30 +0700

    finished mission_local.

--- app/models/MissionLocal.php
+++ app/models/MissionLocal.php
@@ -16,4 +16,20 @@ class MissionLocal extends CMissionLocal{
         $model->scene_id=$sceneId;
         $model->save(false);
     }
+    public static function updateBySceneId($data,$sceneId){
+        $model = self::find()->where(['scene_id'=>$sceneId])->one();
+        foreach ($data as $key => $value){
+            foreach($value as $request){
+                if($request['name']){
+                    $model->name = (string) $request['name'];
+                    $model->save();
+                    
+                    Databasedata::updateXML($request->DatabaseData,$model->databasedata_id);
+                    Progzone::updateXML($request->ProgZone, $model->progzone_id);
+                    Definition::updateXML($request->Definition, $model->definition_id);
+                    Criteria::updateXML($request->Criteria, $model->criteria_id);
+                }
+            }
+        }
+    }
 }
