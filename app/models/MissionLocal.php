2dc2755668eb3221267d386bcc4eb2eb1e52e204
Prawee Wongsa <prawee@hotmail.com>
2014-07-24 19:46:42 +0700

    finished insertBlank

--- app/models/MissionLocal.php
+++ app/models/MissionLocal.php
@@ -9,6 +9,10 @@ use common\models\MissionLocal as CMissionLocal;
 class MissionLocal extends CMissionLocal{
     public static function insertBySceneId($sceneId){
         $model=new self;
+        $model->databasedata_id=(int)  Databasedata::insertGetId();
+        $model->progzone_id=(int)  Progzone::insertGetId();
+        $model->definition_id=(int) Definition::insertGetId();
+        $model->criteria_id=(int)  Criteria::insertGetId();
         $model->scene_id=$sceneId;
         $model->save(false);
     }
