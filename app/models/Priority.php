ff99604b9bc8824293e031b0edf48e711b09234d
Prawee Wongsa <prawee@hotmail.com>
2014-07-24 22:10:44 +0700

    added getIdByValue

---
+++ app/models/Priority.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * 20140724
+ * prawee@hotmail.com
+ */
+namespace app\models;
+
+use common\models\Priority as CPriority;
+class Priority extends CPriority{
+    public static function getIdByValue($value){
+        $model=self::find()->where(['value'=>(string)trim($value)])->one();
+        return $model->id;
+    }
+}
+
