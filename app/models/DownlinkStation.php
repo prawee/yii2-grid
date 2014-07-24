38c070c855c7ea125c2a6ecf7e538a07abe1acf2
Prawee Wongsa <prawee@hotmail.com>
2014-07-24 21:34:54 +0700

    added getIdByValue

--- app/models/DownlinkStation.php
+++ app/models/DownlinkStation.php
@@ -6,5 +6,11 @@
 namespace app\models;
 
 use common\models\DownlinkStation as CDownlinkStation;
-class DownlinkStation extends CDownlinkStation{}
+class DownlinkStation extends CDownlinkStation{
+    
+    public static function getIdByValue($value){
+        $model = self::find()->where(['value'=>$value])->one();
+        return $model->id;
+    }
+}
 
