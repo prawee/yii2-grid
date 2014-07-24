e3354338b69d609ff7639dc4722eb752fc6410e4
Prawee Wongsa <prawee@hotmail.com>
2014-07-24 14:35:09 +0700

    update index xml.

--- app/views/xml/index.php
+++ app/views/xml/index.php
@@ -2,39 +2,66 @@
 
 use yii\helpers\Html;
 use yii\grid\GridView;
-
-/* @var $this yii\web\View */
-/* @var $searchModel app\models\XmlSearch */
-/* @var $dataProvider yii\data\ActiveDataProvider */
+use kartik\icons\Icon;
+Icon::map($this);
+use yii\bootstrap\Modal;
+use yii\widgets\Pjax;
 
 $this->title = 'Xmls';
-$this->params['breadcrumbs'][] = $this->title;
+//$this->params['breadcrumbs'][] = $this->title;
+Modal::begin([
+    'id' =>'content-modal',
+    'header' => Icon::show('star') . '<b>XML Import</b>',
+    'closeButton'=>[
+        'aria-hidden' =>'true',
+        'class'=>'hide',
+    ]
+]);
 ?>
 <div class="xml-index">
-
-    <h1><?= Html::encode($this->title) ?></h1>
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
     <p>
-        <?= Html::a('Create Xml', ['create'], ['class' => 'btn btn-success']) ?>
+        <?= Html::a(Icon::show('plus').' Create', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
-
-    <?= GridView::widget([
+    <?php Pjax::begin(['enablePushState'=>false]); ?>
+    <?=
+    GridView::widget([
         'dataProvider' => $dataProvider,
         'filterModel' => $searchModel,
         'columns' => [
             ['class' => 'yii\grid\SerialColumn'],
-
-            'id',
             'name',
-            'path',
+            //'path',
             'send_email:email',
-            'user_id',
+            //'user_id',
             // 'scene_id',
-            // 'status',
-
-            ['class' => 'yii\grid\ActionColumn'],
+            'status',
+            [
+                'class' => 'prawee\grid\ActionColumn',
+                'template'=>'{import}',
+                'buttons'=>[
+                    'import' => function($url,$model) {
+                        return Html::a(Icon::show('download'),'#',[
+                            'data-pjax'=>'0',
+                            'title'=>' Import ',
+                            'class'=>'btn btn-xs btn-info',
+                        ]);
+                    }
+                ]
+            ],
+            [
+                'class' => 'prawee\grid\ActionColumn',
+                'template'=>'{delete}',
+            ],
         ],
-    ]); ?>
-
+    ]);
+    ?>
+    <?php Pjax::end()?>
+    <?= Html::a(Icon::show('times-circle').'Close',['/uss/index'],[
+        'class' => 'btn btn-danger', 
+        'name' => 'assign-button',
+    ]) ?>
 </div>
+<?php
+Modal::end();
