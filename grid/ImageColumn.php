402845d0ff529673dc3f97d3d72f17cfab3403bd
Prawee Wongsa <prawee@hotmail.com>
2014-07-25 15:46:29 +0700

    fixed null image.

--- grid/ImageColumn.php
+++ grid/ImageColumn.php
@@ -11,10 +11,20 @@ class ImageColumn extends \yii\grid\Column {
     public $attribute='';
     public $path='';
     public $options=[];
+    public $thumbnail=false;
+    public function init() {
+        if($this->thumbnail){
+            $this->options=array_merge($this->options,['class' => 'img-thumbnail',]);
+        }
+    }
     public function renderDataCellContent($model){
-        $image=$model->{$this->attribute};
-        $path=Yii::$app->getUrlManager()->getBaseUrl().DIRECTORY_SEPARATOR.$this->path.DIRECTORY_SEPARATOR.$image;
-        return Html::img($path,$this->options);
+        if(!empty($model->{$this->attribute})){
+            $image=$model->{$this->attribute};
+            $path=Yii::$app->getUrlManager()->getBaseUrl().DIRECTORY_SEPARATOR.$this->path.DIRECTORY_SEPARATOR.$image;
+            return Html::img($path,$this->options);
+        }else{
+            return Html::img('http://placehold.it/40x20',$this->options);
+        }
     }
 }
 
