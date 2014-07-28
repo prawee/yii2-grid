ae51239edaf15d19689c1579dc2232331114c67a
Prawee Wongsa <prawee@hotmail.com>
2014-07-28 11:39:01 +0700

    fixed getting ref variable

--- grid/ActionChildColumn.php
+++ grid/ActionChildColumn.php
@@ -16,7 +16,7 @@ class ActionChildColumn extends \yii\grid\ActionColumn {
         if (!isset($this->buttons['view'])) {
             $this->buttons['view'] = function ($url, $model) {
                 return Html::a('<span class="glyphicon glyphicon-eye-open btn btn-xs btn-primary"></span>',[
-                    'view','id'=>$model->id,'ref'=>Yii::$app->getRequest()->get('ref')
+                    'view','id'=>$model->id,'ref'=>Yii::$app->getRequest()->get('id')
                 ], [
                             'title' => Yii::t('yii', 'View'),
                             'data-pjax' => '0',
@@ -26,7 +26,7 @@ class ActionChildColumn extends \yii\grid\ActionColumn {
         if (!isset($this->buttons['update'])) {
             $this->buttons['update'] = function ($url, $model) {
                 return Html::a('<span class="glyphicon glyphicon-pencil btn btn-xs btn-warning"></span>',[
-                    'update','id'=>$model->id,'ref'=>Yii::$app->getRequest()->get('ref')
+                    'update','id'=>$model->id,'ref'=>Yii::$app->getRequest()->get('id')
                 ], [
                             'title' => Yii::t('yii', 'Update'),
                             'data-pjax' => '0',
@@ -36,7 +36,7 @@ class ActionChildColumn extends \yii\grid\ActionColumn {
         if (!isset($this->buttons['delete'])) {
             $this->buttons['delete'] = function ($url, $model) {
                 return Html::a('<span class="glyphicon glyphicon-trash btn btn-xs btn-danger"></span>',[
-                    'delete','id'=>$model->id,'ref'=>Yii::$app->getRequest()->get('ref')
+                    'delete','id'=>$model->id,'ref'=>Yii::$app->getRequest()->get('id')
                 ], [
                             'title' => Yii::t('yii', 'Delete'),
                             'data-confirm' => Yii::t('yii', 'Are you sure you want to delete this item?'),
