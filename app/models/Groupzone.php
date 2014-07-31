c6c0e5357ffef8d99f4caf93ddb45612f0a0e932
Prawee Wongsa <prawee@hotmail.com>
2014-07-31 22:07:35 +0700

    update insertGetId

--- app/models/Groupzone.php
+++ app/models/Groupzone.php
@@ -7,11 +7,22 @@ namespace app\models;
 
 use common\models\Groupzone as CGroupzone;
 class Groupzone extends CGroupzone{
-    public static function insertGetId(){
+    public static function insertNullGetId(){
         $model=new self;
         $model->save(false);
         return $model->id;
     }
+    public static function insertGetId(){
+        $model=new self;
+        $model->miseo_name=(string)$data->GroupName;
+        $model->info_1=(string)$data->info_1;
+        $model->info_2=(string)$data->info_2;
+        $model->info_3=(string)$data->info_3;
+        $model->info_4=(string)$data->info_4;
+        $model->save();
+        return $model->id;
+    }
+
     public static function updateXML($data,$id){
         $model = self::findOne($id);
         if($model->id){
