2dd53f4df51cbf7e25d62182a1c138bc8802bc1c
Prawee Wongsa <prawee@hotmail.com>
2014-08-01 00:48:15 +0700

    added insertGetId

--- app/models/Definition.php
+++ app/models/Definition.php
@@ -6,11 +6,26 @@
 namespace app\models;
 use common\models\Definition as CDefinition;
 class Definition extends CDefinition{
-    public static function insertGetId(){
+    public static function insertNullGetId(){
         $model=new self;
         $model->save(false);
         return $model->id;
     }
+    public static function insertGetId($data){
+        $model=new self;
+        $model->user_id=(string) (isset($data->Id)?0:$data->Id);
+        $model->user_coordinates=(string) (isset($data->Coordinates)?0:$data->Coordinates);
+        $model->miseo_comment=(string)$data->Comment;
+        $model->deposit_date=(string)$model->getDateTime($data->DepositDate);
+        $model->start_date=(string)$model->getDateTime($data->StartDate);
+        $model->end_date=(string)$model->getDateTime($data->EndDate);
+        $model->completion_date=(string)$model->getDateTime($data->CompletionDate);
+        $model->periodicity_flag=(string)$model->getString($data->Periodicity);
+        $model->priority_id=(int)  Priority::getIdByValue((string)$data->Priority);
+        $model->save();
+        return $model->id;
+    }
+
     public static function updateXML($data,$id){
         $model = self::findOne($id);
         if($model->id){
