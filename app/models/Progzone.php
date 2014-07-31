d73301b950d8faaf6ec3e62bce82dbff70585f7e
Prawee Wongsa <prawee@hotmail.com>
2014-08-01 00:45:28 +0700

    added insertGetId

--- app/models/Progzone.php
+++ app/models/Progzone.php
@@ -8,11 +8,28 @@ namespace app\models;
 use common\models\Progzone as CProgzone;
 use app\models\DownlinkStation;
 class Progzone extends CProgzone{
-    public static function insertGetId(){
+    public static function insertNullGetId(){
         $model=new self;
         $model->save(false);
         return $model->id;
     }
+    public static function insertGetId($data){
+        $model=new self;
+        $model->miseo_name=(string)$data->ReqName;
+        $model->satellite=(int)$data->ReqSatellite;
+        $model->phase=(int)$data->ReqOrbitPhase;
+        $model->average_altitude=(int)$data->AvgAltitude;
+        $model->miseo_template=(string) (isset($data->Template)?$data->Template:'|');
+        $model->center_latitude=(float)isset($data->ReqCenter->Latitude);
+        $model->center_longitude=(float)isset($data->ReqCenter->Longitude);
+        $model->item_length=(int)$data->ReqLength;
+        $model->zonetype=(string)$data->ZoneType;
+        $model->request_status_id=(int)$data->ReqStatus;
+        $model->downlink_station_id=(int)DownlinkStation::getIdByValue((int)$data->DownLinkStation);
+        $model->save();
+        return $model->id;
+    }
+
     public static function updateXML($data,$id){
         //echo '<pre>'.print_r($data,true).'</pre>';
         $model = self::findOne($id);
