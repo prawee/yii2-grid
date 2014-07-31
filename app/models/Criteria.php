660c2c83a16451b34030d3be07c7366e28a74fea
Prawee Wongsa <prawee@hotmail.com>
2014-08-01 00:50:06 +0700

    added insertGetId

--- app/models/Criteria.php
+++ app/models/Criteria.php
@@ -7,11 +7,22 @@ namespace app\models;
 
 use common\models\Criteria as CCriteria;
 class Criteria extends CCriteria{
-    public static function insertGetId(){
+    public static function insertNullGetId(){
         $model=new self;
         $model->save(false);
         return $model->id;
     }
+    public static function insertGetId($data){
+        $model=new self;
+        $model->updatable_gains=(string)trim($data->UpdatableGAINS);
+        $model->sensor_type=(string)trim($data->SensorType);
+        $model->nadir_viewing=(string)trim($model->getString($data->PerfoDomain));
+        $model->compression_ratio=(string)trim($data->Compression);
+        $model->luminosity=(string)trim($model->getString($data->Luminosity));
+        $model->save();
+        return $model->id;
+    }
+
     public static function updateXML($data,$id){
         $model = self::findOne($id);
         if($model->id){
