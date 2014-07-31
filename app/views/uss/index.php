e25a355a7d8c01151dd117158dc5e752f5919919
Prawee Wongsa <prawee@hotmail.com>
2014-08-01 03:21:59 +0700

    update orbit.

--- app/views/uss/index.php
+++ app/views/uss/index.php
@@ -76,7 +76,7 @@ $this->params['breadcrumbs'][] = $this->title;
                     'attribute' => 'id',
                     'value' => function($data) {
                         $date=MissionLocal::find()->where(['scene_id'=>$data->id])->one()->definition->deposit_date;
-                        return $date;
+                        return isset($date);
                     }
                 ],
 //                [
