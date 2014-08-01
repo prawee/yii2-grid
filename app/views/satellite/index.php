5b752fc8a18866fe0d44fa4c7707382f3cbc4ee1
Prawee Wongsa <prawee@hotmail.com>
2014-08-01 11:56:47 +0700

    fake data.

--- app/views/satellite/index.php
+++ app/views/satellite/index.php
@@ -24,6 +24,7 @@ $this->params['breadcrumbs'][] = $this->title;
             'id' => 'Pjax',
             'enablePushState' => false
         ]);
+        //echo '<pre>'.print_r($dataProvider,true).'</pre>';
         echo GridView::widget([
             'dataProvider' => $dataProvider,
             //'filterModel' => $searchModel,
@@ -33,22 +34,58 @@ $this->params['breadcrumbs'][] = $this->title;
                 [
                     'attribute'=>'id',
                     'label'=>'Downlink station',
-                    'value'=>function($data){return null;},
+                    'value'=>function($data){
+                        switch($data->id){
+                            case '30': $d='Bangkok'; break;
+                            case '21': $d='Russia1'; break;
+                            case '20': $d='Sanya'; break;
+                            case '17': $d='Japan'; break;
+                            default : $d='Miyun'; break;
+                        }
+                        return $d;
+                    },
                 ],
                 [
                     'attribute'=>'id',
                     'label'=>'Number of Files',
-                    'value'=>function($data){return null;},
+                    'value'=>function($data){
+                        switch($data->id){
+                            case '30': $d=55; break;
+                            case '21': $d=31; break;
+                            case '20': $d=20; break;
+                            case '17': $d=17; break;
+                            default : $d=108; break;
+                        }
+                        return $d;
+                    },
                 ],
                 [
                     'attribute'=>'id',
                     'label'=>'Number of Scenes',
-                    'value'=>function($data){return null;},
+                    'value'=>function($data){
+                        switch($data->id){
+                            case '30': $d=3; break;
+                            case '21': $d=5; break;
+                            case '20': $d=7; break;
+                            case '17': $d=8; break;
+                            default : $d=9; break;
+                        }
+                        return $d;
+                    },
                 ],
                 [
                     'attribute'=>'id',
                     'label'=>'Total Time(Secounds)',
-                    'value'=>function($data){return null;},
+                    'value'=>function($data){
+                        switch($data->id){
+                            case '30': $d=1230; break;
+                            case '21': $d=5550; break;
+                            case '20': $d=3321; break;
+                            case '17': $d=8831; break;
+                            default : $d=10211; break;
+                        }
+                        return $d;
+                    },
                 ],   
                 [
                     'attribute'=>'id',
