3fa5543c048c7b049b28fe1baf6a31de167746a6
Prawee Wongsa <prawee@hotmail.com>
2014-08-03 19:06:41 +0700

    update create & update  xmldailyplan.

--- app/views/xmldailyplan/create.php
+++ app/views/xmldailyplan/create.php
@@ -1,21 +1,16 @@
 <?php
 
-use yii\helpers\Html;
-
-
-/* @var $this yii\web\View */
-/* @var $model app\models\Xml */
+//use yii\helpers\Html;
 
 $this->title = 'Create Xml';
-$this->params['breadcrumbs'][] = ['label' => 'Xmls', 'url' => ['index']];
-$this->params['breadcrumbs'][] = $this->title;
+//$this->params['breadcrumbs'][] = ['label' => 'Xmls', 'url' => ['index']];
+//$this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="xml-create">
-
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <?= $this->render('_form', [
+    <?=
+    $this->render('_form', [
         'model' => $model,
-    ]) ?>
+    ])
+    ?>
 
 </div>
--- app/views/xmldailyplan/update.php
+++ app/views/xmldailyplan/update.php
@@ -1,21 +1,17 @@
 <?php
 
-use yii\helpers\Html;
-
-/* @var $this yii\web\View */
-/* @var $model app\models\Xml */
+//use yii\helpers\Html;
 
 $this->title = 'Update Xml: ' . ' ' . $model->name;
-$this->params['breadcrumbs'][] = ['label' => 'Xmls', 'url' => ['index']];
-$this->params['breadcrumbs'][] = ['label' => $model->name, 'url' => ['view', 'id' => $model->id]];
-$this->params['breadcrumbs'][] = 'Update';
+//$this->params['breadcrumbs'][] = ['label' => 'Xmls', 'url' => ['index']];
+//$this->params['breadcrumbs'][] = ['label' => $model->name, 'url' => ['view', 'id' => $model->id]];
+//$this->params['breadcrumbs'][] = 'Update';
 ?>
 <div class="xml-update">
-
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <?= $this->render('_form', [
+    <?=
+    $this->render('_form', [
         'model' => $model,
-    ]) ?>
+    ])
+    ?>
 
 </div>
