cdb50c19f822417ba44869bcdd98efdae5225f80
Prawee Wongsa <prawee@hotmail.com>
2014-08-03 17:03:17 +0700

    update index.

--- app/views/xmldailyplan/index.php
+++ app/views/xmldailyplan/index.php
@@ -2,39 +2,88 @@
 
 use yii\helpers\Html;
 use yii\grid\GridView;
+use kartik\icons\Icon;
+Icon::map($this);
+use yii\bootstrap\Modal;
+use yii\widgets\Pjax;
 
-/* @var $this yii\web\View */
-/* @var $searchModel app\models\XmlSearch */
-/* @var $dataProvider yii\data\ActiveDataProvider */
 
 $this->title = 'Xmls';
-$this->params['breadcrumbs'][] = $this->title;
+//$this->params['breadcrumbs'][] = $this->title;
+
+Modal::begin([
+    'id' =>'content-modal',
+    'header' => Icon::show('star') . '<b>XML Import</b>',
+    'closeButton'=>[
+        'aria-hidden' =>'true',
+        'class'=>'hide',
+    ]
+]);
 ?>
 <div class="xml-index">
-
-    <h1><?= Html::encode($this->title) ?></h1>
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
     <p>
-        <?= Html::a('Create Xml', ['create'], ['class' => 'btn btn-success']) ?>
+        <?= Html::a(Icon::show('plus').'Create', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
 
-    <?= GridView::widget([
+    <?=
+    GridView::widget([
         'dataProvider' => $dataProvider,
         'filterModel' => $searchModel,
         'columns' => [
             ['class' => 'yii\grid\SerialColumn'],
-
-            'id',
-            'name',
-            'path',
-            'user_id',
-            'scene_id',
-            // 'status',
-            // 'xml_type_id',
-
-            ['class' => 'yii\grid\ActionColumn'],
+            [
+                'attribute'=>'name',
+                'label'=>'XML Name',
+            ],
+            [
+                'attribute' => 'status',
+                'label'=>'Import Status',
+                'filter' => [0 => 'Not Import', 1 => 'Import Complete'],
+                'value' => function($data) {
+                    return ($data->status == 1 ? 'Import Complete' : 'Not Import');
+                },
+                'options' => [
+                    'style' => 'width:200px;'
+                ]
+            ],
+            [
+                'class' => 'prawee\grid\ActionColumn',
+                'template'=>'{import} {delete}',
+                'buttons'=>[
+                    'import' => function($url,$model) {
+                        if($model->status===0){
+                            $ref=Yii::$app->getRequest()->get('id');
+                            $type=Yii::$app->getRequest()->get('type');
+                            $url=['import','id'=>$model->id,'ref'=>$ref,'type'=>$type];
+                            return Html::a('<span class="glyphicon glyphicon-upload btn btn-xs btn-success"></span>',$url,[
+                                'data-pjax'=>'0',
+                                'title'=>' Import ',
+                            ]);
+                            
+                        }else{
+                            return '<span class="label btn btn-xs btn-warning">Yes</span>';
+                        }
+                    },
+                    'delete'=>function($url,$model){
+                        $ref=Yii::$app->getRequest()->get('id');
+                        $type=Yii::$app->getRequest()->get('type');
+                        $url=['delete','id'=>$model->id,'ref'=>$ref,'type'=>$type];
+                        return Html::a('<span class="glyphicon glyphicon-trash btn btn-xs btn-danger"></span>', $url, [
+                            'title' => Yii::t('yii', 'Delete'),
+                            'data-confirm' => Yii::t('yii', 'Are you sure you want to delete this item?'),
+                            'data-method' => 'post',
+                            'data-pjax' => '0',
+                        ]);
+                    }
+                ]
+                
+            ],
         ],
-    ]); ?>
+    ]);
+    ?>
 
 </div>
+<?php
+Modal::end();
