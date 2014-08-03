8afd30e717c41e73a7a746a30b14cf0b0d7929c8
Playwork <Playwork@Mobile-TEAM01>
2014-08-03 17:18:05 +0700

    #เปลี่ยน label name -      Attr s -> Sensor
    -      Is Rush -> Rush

--- app/models/USSWo.php
+++ app/models/USSWo.php
@@ -1,32 +1,49 @@
 <?php
+
 /*
  * 2014-07-20
  * prawee@hotmail.com
  */
+
 namespace app\models;
 
 use Yii;
 use \yii\db\ActiveRecord;
-class USSWo extends ActiveRecord{
+
+class USSWo extends ActiveRecord {
+
     public static function getDb() {
         return Yii::$app->db3;
     }
+
     public static function tableName() {
         return 'tpt_wo';
     }
+
     public static function primaryKey() {
         return ['id'];
     }
-    public function getDepositDate(){
-        $date=MissionLocal::find()->where(['scene_id'=>$this->id])->one();
-        return is_object($date)?$date->definition->deposit_date:null;
+
+    public function getDepositDate() {
+        $date = MissionLocal::find()->where(['scene_id' => $this->id])->one();
+        return is_object($date) ? $date->definition->deposit_date : null;
     }
-    public function getStripName(){
-        $model=SplittedStripLocal::find()->where(['scene_id'=>$this->id])->one();
-        return is_object($model)?$model->strips->miseo_name:null;
+
+    public function getStripName() {
+        $model = SplittedStripLocal::find()->where(['scene_id' => $this->id])->one();
+        return is_object($model) ? $model->strips->miseo_name : null;
     }
-    public function getRequestName(){
-        $model=MiseoGroupLocal::find()->where(['scene_id'=>$this->id])->one(); 
-        return is_object($model)?$model->name:null;
+
+    public function getRequestName() {
+        $model = MiseoGroupLocal::find()->where(['scene_id' => $this->id])->one();
+        return is_object($model) ? $model->name : null;
     }
+
+    public function attributeLabels() {
+        return [
+            'Attr S' => 'Sensor',
+            'Is Rush' => 'Rush',    
+        ];
+    }
+
 }
