72bf158750728cb039839efb70142889ef401276
Prawee Wongsa <prawee@hotmail.com>
2014-08-04 00:50:52 +0700

    check data before insert.

--- app/models/Strips.php
+++ app/models/Strips.php
@@ -13,24 +13,27 @@ class Strips extends CStrips{
         return $model->id;
     }
     public static function insertGetId($data){
-        $model=new self;
-        $model->miseo_name=(string)$data->StripName;
-        $model->rank=(int)$data->Rank;
-        $model->lat_center=isset($data->StripCenter->Latitude)?(string)$data->StripCenter->Latitude:null;
-        $model->lon_center=isset($data->StripCenter->Longitude)?(string)$data->StripCenter->Longitude:null;
-        $model->strip_length=isset($data->StripLength)?(string)$data->StripLength:null;
-        $model->strip_width=isset($data->StripWidth)?(string)$data->StripWidth:null;
-        $model->lat_corner_nw=isset($data->CornerNW->Latitude)?(string)$data->CornerNW->Latitude:null;
-        $model->lon_corner_nw=isset($data->CornerNW->Longitude)?(string)$data->CornerNW->Longitude:null;
-        $model->lat_corner_ne=isset($data->CornerNE->Latitude)?(string)$data->CornerNE->Latitude:null;
-        $model->lon_corner_ne=isset($data->CornerNE->Longitude)?(string)$data->CornerNE->Longitude:null;
-        $model->lat_corner_se=isset($data->CornerSE->Latitude)?(string)$data->CornerSE->Latitude:null;
-        $model->lon_corner_se=isset($data->CornerSE->Longitude)?(string)$data->CornerSE->Longitude:null;
-        $model->lat_corner_sw=isset($data->CornerSW->Latitude)?(string)$data->CornerSW->Latitude:null;
-        $model->lon_corner_sw=isset($data->CornerSW->Longitude)?(string)$data->CornerSW->Longitude:null;
-        $model->strip_status_id=(int)$data->StripStatus;
-        $model->save(false);
-        return $model->id;
+        if (is_object($data)) {
+            $model = new self;
+            $model->miseo_name = (string) $data->StripName;
+            $model->rank = (int) $data->Rank;
+            $model->lat_center = isset($data->StripCenter->Latitude) ? (string) $data->StripCenter->Latitude : null;
+            $model->lon_center = isset($data->StripCenter->Longitude) ? (string) $data->StripCenter->Longitude : null;
+            $model->strip_length = isset($data->StripLength) ? (string) $data->StripLength : null;
+            $model->strip_width = isset($data->StripWidth) ? (string) $data->StripWidth : null;
+            $model->lat_corner_nw = isset($data->CornerNW->Latitude) ? (string) $data->CornerNW->Latitude : null;
+            $model->lon_corner_nw = isset($data->CornerNW->Longitude) ? (string) $data->CornerNW->Longitude : null;
+            $model->lat_corner_ne = isset($data->CornerNE->Latitude) ? (string) $data->CornerNE->Latitude : null;
+            $model->lon_corner_ne = isset($data->CornerNE->Longitude) ? (string) $data->CornerNE->Longitude : null;
+            $model->lat_corner_se = isset($data->CornerSE->Latitude) ? (string) $data->CornerSE->Latitude : null;
+            $model->lon_corner_se = isset($data->CornerSE->Longitude) ? (string) $data->CornerSE->Longitude : null;
+            $model->lat_corner_sw = isset($data->CornerSW->Latitude) ? (string) $data->CornerSW->Latitude : null;
+            $model->lon_corner_sw = isset($data->CornerSW->Longitude) ? (string) $data->CornerSW->Longitude : null;
+            $model->strip_status_id = (int) $data->StripStatus;
+            $model->save(false);
+            return $model->id;
+        }
+        return null;
     }
 
     public static function updateXML($data,$id){
