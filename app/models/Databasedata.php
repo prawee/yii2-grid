3e289d3e65c36d4c81ba3b5c33ae9a05d352f92f
Prawee Wongsa <prawee@hotmail.com>
2014-08-04 00:34:51 +0700

    update insertGetId by check data before insert , strip_satellite,strip_orbit_phase

--- app/models/Databasedata.php
+++ app/models/Databasedata.php
@@ -14,18 +14,28 @@ class Databasedata extends CDatabasedata{
         return $model->id;
     }
     public static function insertGetId($data){
-        $model=new self;
-        $model->name=(string)$data->Name;
-        $model->miseo_reference=(string)$data->ReferenceMiseo;
-        $model->miseo_group=(string)$data->Group;
-        $model->miseo_template=(string)$data->Template;
-        $model->nb_summits_cov=(int)$data->StripNb;
-        $model->stereo_type=(string)isset($data->StereoType->NA);
-        $model->organism=(string) $data->Organism;
-        $model->delta_lat_north=isset($data->DeltaLatNorth)?(string)$data->DeltaLatNorth:null;
-        $model->delta_lat_south=isset($data->DeltaLatSouth)?(string)$data->DeltaLatSouth:null;
-        $model->save();
-        return $model->id;
+        if (is_object($data)) {
+            $model = new self;
+            $model->name = (string) $data->Name;
+            $model->miseo_reference = (string) $data->ReferenceMiseo;
+            $model->miseo_group = (string) $data->Group;
+            $model->miseo_template = (string) $data->Template;
+            $model->nb_summits_cov = (int) $data->StripNb;
+            $model->stereo_type = (string) isset($data->StereoType->NA);
+            $model->organism = (string) $data->Organism;
+            $model->delta_lat_north = isset($data->DeltaLatNorth) ? (string) $data->DeltaLatNorth : null;
+            $model->delta_lat_south = isset($data->DeltaLatSouth) ? (string) $data->DeltaLatSouth : null;
+            
+            if($data->StripSatellite){
+                $model->strip_satellite=(int)$data->StripSatellite;
+            }
+            if($data->StripOrbitPhase){
+                $model->strip_orbit_phase=(int)$data->StripOrbitPhase;
+            }
+            $model->save();
+            return $model->id;
+        }
+        return null;
     }
 
     public static function updateXML($data,$id){
