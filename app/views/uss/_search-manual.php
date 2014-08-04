0b93f562e222e57c74ec03804d025da2cee66620
Prawee Wongsa <prawee@hotmail.com>
2014-08-05 03:35:25 +0700

    draft search request

--- app/views/uss/_search-manual.php
+++ app/views/uss/_search-manual.php
@@ -1,39 +1,83 @@
-<?php 
-//use kartik\widgets\DatePicker;
+<?php
+
+//use yii\jui\DatePicker;
+use kartik\widgets\DatePicker;
+//use zhuravljov\widgets\DatePicker;
 use yii\bootstrap\ButtonGroup;
+//use yii\bootstrap\Button;
 use kartik\icons\Icon;
+use yii\bootstrap\Dropdown;
+//use yii\widgets\InputWidget;
+
 Icon::map($this);
 ?>
 
 <div class='uss-filter'>
-    <div class="col-xs-4 well well-sm">
+    <div class="col-md-5 well well-sm">
         <b>Select Data</b>
-    </div>
-    <div class="col-xs-4 well well-sm">
-        <b>Data Selection</b>
-    </div>
-    <div class="col-xs-4 well well-sm text-center">
-        <p class="text-left"><b>Tool</b></p>
         <p style="margin-top:15px;">
-            <?php
-            echo ButtonGroup::widget([
-                'buttons' => [
-                    //Button::widget(['label' => 'A']),
-                    [
-                        'label' => Icon::show('times') . 'Remove',
-                        'options' => ['class' => 'btn-default']
-                    ], [
-                        'label' => Icon::show('eye') . 'View',
-                        'options' => ['class' => 'btn-default']
-                    ], [
-                        'label' => Icon::show('edit') . 'Change Status',
-                        'options' => ['class' => 'btn-default']
-                    ],
-                ],
-                'encodeLabels' => false,
-            ]);
-            ?>
+           
         </p>
+    </div>
+    <div class="col-md-7">
+        <div class="row">
+            <div class="col-md-6 well well-sm">
+                <p class="text-left"><b>Data Selection</b></p>
+                <p style="margin-top:15px;">
+                    <?php
+                    $picker=DatePicker::widget([
+                                'name' => 'date',
+                                'options' => [
+                                    'style'=>'width:70px;'
+                                ]
+                            ]);
+                    echo ButtonGroup::widget([
+                        'options' => ['class' => 'col-xs-12'],
+                        'buttons' => [
+                            [
+                                'label' => 'All Day', 
+                                'options' => [
+                                    'class' => 'btn-default col-xs-3'
+                                ]
+                            ],
+                            [
+                                'label' => 'Choose Date',
+                                'options' => [
+                                    'class' => 'btn-default col-xs-4'
+                                ]
+                            ],
+                            $picker
+                        ],
+                        'encodeLabels' => false,
+                    ]);
+                    
+                    ?>
+                </p>
+            </div>
+            <div class="col-md-6 well well-sm text-center">
+                <p class="text-left"><b>Tool</b></p>
+                <p style="margin-top:15px;">
+                    <?php
+                    echo ButtonGroup::widget([
+                        //'options' => ['class' => 'col-sm-12'],
+                        'buttons' => [
+                            [
+                                'label' => Icon::show('times') . 'Remove',
+                                'options' => ['class' => 'btn-default']
+                            ], [
+                                'label' => Icon::show('eye') . 'View',
+                                'options' => ['class' => 'btn-default']
+                            ], [
+                                'label' => Icon::show('edit') . 'Change Status',
+                                'options' => ['class' => 'btn-default']
+                            ],
+                        ],
+                        'encodeLabels' => false,
+                    ]);
+                    ?>
+                </p>
+            </div>
+        </div>
     </div>
 </div>
 
