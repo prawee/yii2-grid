47e94d6de71ab49e84af916a8847c7d4a8ad0eea
Prawee Wongsa <prawee@hotmail.com>
2014-08-04 18:02:35 +0700

    fixed temp data of dailyplan.

--- app/views/dailyplan/index.php
+++ app/views/dailyplan/index.php
@@ -13,7 +13,7 @@ $this->params['breadcrumbs'][] = $this->title;
 <?php echo $this->render('_search-manual', ['model' => $searchModel]);  ?>
 <div class="clearfix"></div>
 <div class="plan-local-index">
-
+    <div class="scrollspy-board" data-spy="scroll" data-offset="0">
     <?php Pjax::begin(['enablePushState' => false]); ?>
     <?=
     GridView::widget([
@@ -27,6 +27,25 @@ $this->params['breadcrumbs'][] = $this->title;
                 'attribute' => 'start_date',
                 'header' => 'Plan Date Start',
             ],
+            ['attribute'=>'id','value'=>function($e){return null;},'header'=>'Request Name'],
+            ['attribute'=>'id','value'=>function($e){return null;},'header'=>'Strip'],
+            ['attribute'=>'id','value'=>function($e){return null;},'header'=>'Orbit'],
+            ['attribute'=>'id','value'=>function($e){return null;},'header'=>'PAN File'],
+            ['attribute'=>'id','value'=>function($e){return null;},'header'=>'MS File'],
+            ['attribute'=>'id','value'=>function($e){return null;},'header'=>'Rev No.'],
+            ['attribute'=>'id','value'=>function($e){return null;},'header'=>'Downlink Station'],
+            ['attribute'=>'id','value'=>function($e){return null;},'header'=>'PAN'],
+            ['attribute'=>'id','value'=>function($e){return null;},'header'=>'B1'],
+            ['attribute'=>'id','value'=>function($e){return null;},'header'=>'B2'],
+            ['attribute'=>'id','value'=>function($e){return null;},'header'=>'B3'],
+            ['attribute'=>'id','value'=>function($e){return null;},'header'=>'B4'],
+            ['attribute'=>'id','value'=>function($e){return null;},'header'=>'Comp.Ratio'],
+            ['attribute'=>'id','value'=>function($e){return null;},'header'=>'Roll Max'],
+            ['attribute'=>'id','value'=>function($e){return null;},'header'=>'Pitch'],
+            ['attribute'=>'id','value'=>function($e){return null;},'header'=>'Luminosity'],
+            ['attribute'=>'id','value'=>function($e){return null;},'header'=>'Cloud Coverage'],
+            ['attribute'=>'id','value'=>function($e){return null;},'header'=>'CUF Status'],
+            ['attribute'=>'id','value'=>function($e){return null;},'header'=>'Remark'],
             [
                 'class' => 'prawee\grid\ActionColumn',
                 'header' => 'Action',
@@ -53,4 +72,5 @@ $this->params['breadcrumbs'][] = $this->title;
     ]);
     ?>
     <?php Pjax::end(); ?>
+    </div>
 </div>
