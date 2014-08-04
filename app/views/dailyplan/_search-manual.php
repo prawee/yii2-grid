f53acecbd2be3d5d5a4bddf8568252dae8947827
Prawee Wongsa <prawee@hotmail.com>
2014-08-04 17:28:19 +0700

    added _serach-manual

---
+++ app/views/dailyplan/_search-manual.php
@@ -0,0 +1,51 @@
+<?php
+use yii\helpers\Html;
+//use yii\bootstrap\Button;
+use yii\bootstrap\ButtonGroup;
+use kartik\widgets\DatePicker;
+use kartik\icons\Icon;
+Icon::map($this);
+?>
+<div class="plan-local-filter">
+    <div class="col-xs-2 well well-sm">
+        <b>Upload Plan</b>
+        <p class="" style="margin-top:15px;">
+            <?= Html::a(Icon::show('plus') . 'Upload New Plan', [ 'xmldailyplan/index'], ['class' => 'btn btn-success']) ?>
+        </p>
+    </div>
+    <div class="col-xs-5 well well-sm">
+        <b>Planing Date</b>
+        <p style="margin-top:15px;">
+            <?php
+            echo DatePicker::widget([
+
+                'name' => 'plan_date'
+            ]);
+            ?>
+        </p>
+    </div>
+    <div class="col-xs-5 well-sm well text-center">
+        <p class="text-left"><b>Tool</b></p>
+        <p style="margin-top:15px;">
+            <?php
+            echo ButtonGroup::widget([
+                'buttons' => [
+                    //Button::widget(['label' => 'A']),
+                    [
+                        'label' => Icon::show('save') . 'Shape Current Page',
+                        'options' => ['class' => 'btn-default']
+                    ], [
+                        'label' => Icon::show('upload') . 'Export Current Page',
+                        'options' => ['class' => 'btn-default']
+                    ], [
+                        'label' => Icon::show('times') . 'Remove',
+                        'options' => ['class' => 'btn-default']
+                    ],
+                ],
+                'encodeLabels' => false,
+            ]);
+            ?>
+        </p>
+    </div>
+</div>
+
