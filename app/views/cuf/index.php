d2350724c84713eb0cc5c15cc748a64392625df0
Prawee Wongsa <prawee@hotmail.com>
2014-08-07 02:47:28 +0700

    added filter of cuf.

--- app/views/cuf/index.php
+++ app/views/cuf/index.php
@@ -14,12 +14,9 @@ Icon::map($this);
 $this->title = 'CUF';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
+<?php echo $this->render('_search-manual', ['model' => $searchModel]);   ?>
+<div class="clearfix"></div>
 <div class="usswo-index " >
-    <p>
-        <?= Html::a(Icon::show('plus').' Upload CUF File', ['#'], ['class' => 'btn btn-success']) ?>
-    </p>
-
-    <?php //echo $this->render('_search', ['model' => $searchModel]);   ?>
     <div class="scrollspy-board" data-spy="scroll" data-offset="0">
         <?php
         Pjax::begin([
