36933e8cf10434d44de390d62ddba98c9edb3dbf
Prawee Wongsa <prawee@hotmail.com>
2014-08-08 10:54:55 +0700

    added params variable and build query with url

--- grid/ActionChildColumn.php
+++ grid/ActionChildColumn.php
@@ -12,13 +12,28 @@ use yii\helpers\Html;
 use yii\grid\ActionColumn;
 
 class ActionChildColumn extends ActionColumn {
+    /*
+     * for support multi params
+     * @return array
+     */
+    public $params;
+    /*
+     * 2014-08-08
+     */
+    public function init() {
+        parent::init();
+        if(is_array($this->params)){
+            $this->params='&'.http_build_query($this->params);
+        }else{
+            $this->params='&'.http_build_query(['ref'=>Yii::$app->getRequest()->get('id')]);
+        }
+    }
 
     protected function initDefaultButtons() {
         if (!isset($this->buttons['view'])) {
             $this->buttons['view'] = function ($url, $model) {
-                return Html::a('<span class="glyphicon glyphicon-eye-open btn btn-xs btn-primary"></span>',[
-                    'view','id'=>$model->id,'ref'=>Yii::$app->getRequest()->get('id')
-                ], [
+                $url.=$this->params;
+                return Html::a('<span class="glyphicon glyphicon-eye-open btn btn-xs btn-primary"></span>',$url, [
                             'title' => Yii::t('yii', 'View'),
                             'data-pjax' => '0',
                 ]);
@@ -26,9 +41,8 @@ class ActionChildColumn extends ActionColumn {
         }
         if (!isset($this->buttons['update'])) {
             $this->buttons['update'] = function ($url, $model) {
-                return Html::a('<span class="glyphicon glyphicon-pencil btn btn-xs btn-warning"></span>',[
-                    'update','id'=>$model->id,'ref'=>Yii::$app->getRequest()->get('id')
-                ], [
+                $url.=$this->params;
+                return Html::a('<span class="glyphicon glyphicon-pencil btn btn-xs btn-warning"></span>',$url, [
                             'title' => Yii::t('yii', 'Update'),
                             'data-pjax' => '0',
                 ]);
@@ -36,9 +50,8 @@ class ActionChildColumn extends ActionColumn {
         }
         if (!isset($this->buttons['delete'])) {
             $this->buttons['delete'] = function ($url, $model) {
-                return Html::a('<span class="glyphicon glyphicon-trash btn btn-xs btn-danger"></span>',[
-                    'delete','id'=>$model->id,'ref'=>Yii::$app->getRequest()->get('id')
-                ], [
+                $url.=$this->params;
+                return Html::a('<span class="glyphicon glyphicon-trash btn btn-xs btn-danger"></span>',$url, [
                             'title' => Yii::t('yii', 'Delete'),
                             'data-confirm' => Yii::t('yii', 'Are you sure you want to delete this item?'),
                             'data-method' => 'post',
