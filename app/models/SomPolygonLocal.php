59337cd59d0143a6ed85b3844316ad3116ec9e96
Prawee Wongsa <prawee@hotmail.com>
2014-08-11 21:39:34 +0700

    added extends SomPolygonLocal

---
+++ app/models/SomPolygonLocal.php
@@ -0,0 +1,9 @@
+<?php
+/*
+ * 2014-08-11
+ * konkeanweb@gmail.com
+ */
+namespace app\models;
+
+use common\models\SomPolygonLocal as CSomPolygonLocal;
+class SomPolygonLocal extends CSomPolygonLocal{}
