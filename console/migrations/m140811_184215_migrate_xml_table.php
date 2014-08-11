b25dcc1be77c1beb52d470b464dc0384753e3137
Prawee Wongsa <prawee@hotmail.com>
2014-08-12 01:49:06 +0700

    added migrate xml table.

---
+++ console/migrations/m140811_184215_migrate_xml_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m140811_184215_migrate_xml_table extends Migration {
+
+    public function up() {
+        $this->dropColumn('xml','user_id');
+        $this->addColumn('xml','distributor_id',  Schema::TYPE_INTEGER);
+        $this->addColumn('xml','client_id',  Schema::TYPE_INTEGER);
+        $this->addForeignKey('fk_xml_distributor','xml','distributor_id','user','id','SET NULL','CASCADE');
+        $this->addForeignKey('fk_xml_client','xml','client_id','user','id','SET NULL','CASCADE');
+    }
+
+    public function down() {
+        echo "m140811_184215_migrate_xml_table cannot be reverted.\n";
+
+        return false;
+    }
+
+}
