c1996e3efd0405070feba713b86193dd22b1dd9c
Prawee Wongsa <prawee@hotmail.com>
2014-08-12 03:38:05 +0700

    added distributor_id,client_id with form.

--- app/views/xml/_form.php
+++ app/views/xml/_form.php
@@ -4,6 +4,10 @@ use yii\widgets\ActiveForm;
 use yii\bootstrap\Modal;
 use kartik\icons\Icon;
 Icon::map($this);
+use app\models\User;
+use yii\helpers\ArrayHelper;
+use kartik\widgets\DepDrop;
+use yii\helpers\Url;
 
 
 $id=Yii::$app->getRequest()->get('id');
@@ -25,8 +29,23 @@ Modal::begin([
         'options' => ['enctype' => 'multipart/form-data']
     ]);
     ?>
-    <?= $form->errorSummary($model)?>
+    <?php //$form->errorSummary($model); ?>
     <?= $form->field($model, 'name')->fileInput() ?>
+    <?php
+    $distributorList= ArrayHelper::map(User::find()->where(['role'=>88])->asArray()->all(),'id','username');
+    ?>
+    <?= $form->field($model,'distributor_id')->dropDownList($distributorList,array('prompt'=>'','id'=>'distributor-id'))?>
+    
+    <?php
+    echo $form->field($model, 'client_id')->widget(DepDrop::classname(), [
+        'options' => ['id' => 'client-id'],
+        'pluginOptions' => [
+            'depends' => ['distributor-id'],
+            'placeholder' => 'Select...',
+            'url' => Url::to(['/xml/client'])
+        ]
+    ]);
+    ?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? Icon::show('plus').' Create' : Icon::show('edit').' Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
