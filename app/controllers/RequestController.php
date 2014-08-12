66cecd0032287fcc2810b7cae11eacb61ba3be33
Prawee Wongsa <prawee@hotmail.com>
2014-08-12 19:23:40 +0700

    fixed StripAccessLocal not found;

--- app/controllers/RequestController.php
+++ app/controllers/RequestController.php
@@ -11,6 +11,7 @@ use yii\filters\VerbFilter;
 use auth\Asset;
 use app\models\USSWo;
 use yii\data\ActiveDataProvider;
+use app\models\StripAccessLocal;
 
 /**
  * RequestController implements the CRUD actions for Scene model.
