2a558c2097d13ec7d7eb46c72889354dc2a032a9
Prawee Wongsa <prawee@hotmail.com>
2014-08-12 18:06:59 +0700

    fixed rename column with server.

--- console/migrations/m140811_184215_migrate_xml_table.php
+++ console/migrations/m140811_184215_migrate_xml_table.php
@@ -6,8 +6,9 @@ use yii\db\Migration;
 class m140811_184215_migrate_xml_table extends Migration {
 
     public function up() {
-        $this->dropColumn('xml','user_id');
-        $this->addColumn('xml','client_id',  Schema::TYPE_INTEGER);
+        //$this->renameColumn('xml','user_id','client_id');
+        //$this->dropColumn('xml','user_id');
+        //$this->addColumn('xml','client_id',  Schema::TYPE_INTEGER);
         $this->addColumn('xml','distributor_id',  Schema::TYPE_INTEGER);
         $this->addForeignKey('fk_xml_distributor','xml','distributor_id','user','id','SET NULL','CASCADE');
     }
