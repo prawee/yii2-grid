e7aa547f7ebc6aafa3e3a775e544dca6cecc189a
Prawee Wongsa <prawee@hotmail.com>
2014-08-12 16:49:45 +0700

    finished import strip access local

--- app/models/StripAccessLocal.php
+++ app/models/StripAccessLocal.php
@@ -9,6 +9,7 @@ use common\models\StripAccessLocal as CStripAccessLocal;
 use yii\behaviors\TimestampBehavior;
 use yii\db\ActiveRecord;
 use yii\db\Expression;
+use app\models\MissionLocal;
 class StripAccessLocal extends CStripAccessLocal{
     public function behaviors() {
         return [
@@ -24,29 +25,43 @@ class StripAccessLocal extends CStripAccessLocal{
     }
     public static function insertBySplitted($data,$splittedId,$sceneId){
         //self::deleteAll(['splitted_strip_local_id'=>$id]);
-        echo '<pre>'.print_r($data,true).'</pre>';
+        //echo '<pre>'.print_r($data,true).'</pre>';
         foreach($data as $values){
             foreach($values as $key=>$value){
                 if(isset($value['DBTable'])){
                     //echo '<pre>'.print_r($value,true).'</pre>';
                     $model=new self;
                     $model->splitted_strip_local_id=(int)$splittedId;
-                    $model->miseo_reference=(string)$value->DbData->Ref;
-                    $model->miseo_group=(string)$value->DbData->Grp;
-                    $model->miseo_template=(string)$value->DbData->Tmpl;
-                    $model->orbit_cycle=(string)$value->Orbit;
-                    $model->orbit_cycle_couple=(string)$value->Couple;
-                    $model->roll_max_access=(string)$value->RollMax;
-                    $model->earliest_date=(string)$value->EasliestDate;
+                    $model->attr_status=self::trim($value['status']);
+                    $model->attr_type=self::trim($value['type']);
+                    $model->attr_image=self::trim($value['image']);
+                    $model->attr_name=self::trim($value['name']);
+                    $model->miseo_reference=self::trim($value->DbData->Ref);
+                    $model->miseo_group=self::trim($value->DbData->Grp);
+                    $model->miseo_template=self::trim($value->DbData->Tmpl);
+                    $model->orbit_cycle=self::trim($value->Orbit);
+                    $model->orbit_cycle_couple=self::trim($value->Couple);
+                    $model->roll_max_access=self::trim($value->RollMax);
+                    $model->earliest_date=self::datetime($value->EarliestDate);
                     $model->scene_id=(int)$sceneId;
                     
                     if($model->roll_max_access!=0){
                         $model->save();
                     }
+                    //echo '<pre>'.print_r($model->attributes,true).'</pre>';
                 }
             }
         }
-        exit;
+    }
+    public static function trim($data){
+        return MissionLocal::trim($data);
+    }
+
+    public static function date($date){
+        return MissionLocal::date($date);
+    }
+    public static function datetime($date){
+        return MissionLocal::datetime($date);
     }
 }
 
