5920ee7fb271ef2980a26e81516c727dd95ed5db
Prawee Wongsa <prawee@hotmail.com>
2014-08-13 14:26:21 +0700

    added change-status.

--- js/tpt.js
+++ js/tpt.js
@@ -3,6 +3,11 @@
  * prawee@hotmail.com
  */
 jQuery(document).ready(function() {    
-    
+    $('#changeStatus').click(function(){
+        var id=$("#grid-request").yiiGridView('getSelectedRows');
+        if(id.length){
+            window.location.href='change-status?id='+id;
+        }
+    });
 });
 
