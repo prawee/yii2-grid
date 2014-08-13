73173e9b5fb68184f74069c0ea3a97ffb757962c
Prawee Wongsa <prawee@hotmail.com>
2014-08-13 13:49:57 +0700

    extends request status model.

---
+++ app/models/RequestStatus.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * 2014-08-13
+ * konkeanweb@gmail.com
+ */
+namespace app\models;
+
+use common\models\RequestStatus as CRequestStatus;
+
+class RequestStatus extends CRequestStatus{}
+
