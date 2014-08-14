2bf8471fb52a38762bc4fc008d30ee1f11193852
Prawee Wongsa <prawee@hotmail.com>
2014-08-14 10:55:39 +0700

    remove script.

--- js/tpt.js
+++ js/tpt.js
@@ -3,11 +3,6 @@
  * prawee@hotmail.com
  */
 jQuery(document).ready(function() {    
-    $('#changeStatus').click(function(){
-        var id=$("#grid-request").yiiGridView('getSelectedRows');
-        if(id.length){
-            window.location.href='change-status?id='+id;
-        }
-    });
+    
 });
 
