2950592a423cecaa2d15c0707f31f15e010b897e
Prawee Wongsa <prawee@hotmail.com>
2020-08-14 15:55:03 +0700

    update tooltip placement

--- src/ActionColumn.php
+++ src/ActionColumn.php
@@ -16,8 +16,9 @@ class ActionColumn extends \yii\grid\ActionColumn {
         if (!isset($this->buttons['view'])) {
             $this->buttons['view'] = function ($url, $model) {
                 return Html::a('<span class="fa fa-eye-open btn btn-xs btn-outline-primary"></span>', $url, [
-                            'title' => Yii::t('yii', 'View'),
-                            'data-pjax' => '0',
+                    'title' => Yii::t('yii', 'View'),
+                    'data-pjax' => '0',
+                    'data-placement' => 'bottom',
                     'data-toggle'=>'tooltip'
                 ]);
             };
@@ -25,8 +26,9 @@ class ActionColumn extends \yii\grid\ActionColumn {
         if (!isset($this->buttons['update'])) {
             $this->buttons['update'] = function ($url, $model) {
                 return Html::a('<span class="fa fa-pencil btn btn-xs btn-outline-warning"></span>', $url, [
-                            'title' => Yii::t('yii', 'Update'),
-                            'data-pjax' => '0',
+                    'title' => Yii::t('yii', 'Update'),
+                    'data-pjax' => '0',
+                    'data-placement' => 'bottom',
                     'data-toggle'=>'tooltip'
                 ]);
             };
@@ -34,10 +36,11 @@ class ActionColumn extends \yii\grid\ActionColumn {
         if (!isset($this->buttons['delete'])) {
             $this->buttons['delete'] = function ($url, $model) {
                 return Html::a('<span class="fa fa-trash btn btn-xs btn-outline-danger"></span>', $url, [
-                            'title' => Yii::t('yii', 'Delete'),
-                            'data-confirm' => Yii::t('yii', 'Are you sure you want to delete this item?'),
-                            'data-method' => 'post',
-                            'data-pjax' => '0',
+                    'title' => Yii::t('yii', 'Delete'),
+                    'data-confirm' => Yii::t('yii', 'Are you sure you want to delete this item?'),
+                    'data-method' => 'post',
+                    'data-pjax' => '0',
+                    'data-placement' => 'bottom',
                     'data-toggle'=>'tooltip'
                 ]);
             };
